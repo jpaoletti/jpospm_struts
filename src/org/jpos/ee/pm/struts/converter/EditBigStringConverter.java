645e97912d7795c8f6181e6a41d1639611c562fd
Jeronimo Paoletti <jeronimo.paoletti@gmail.com>
2011-04-30 21:04:51 -0300

    New edit big string converter (textarea)

---
+++ src/org/jpos/ee/pm/struts/converter/EditBigStringConverter.java
@@ -0,0 +1,28 @@
+package org.jpos.ee.pm.struts.converter;
+
+import org.jpos.ee.pm.converter.ConverterException;
+import org.jpos.ee.pm.core.EntityInstanceWrapper;
+import org.jpos.ee.pm.core.Field;
+import org.jpos.ee.pm.core.PMContext;
+
+/**
+ *
+ * @author jpaoletti
+ */
+public class EditBigStringConverter extends EditStringConverter {
+
+    @Override
+    public String visualize(PMContext ctx) throws ConverterException {
+        final EntityInstanceWrapper einstance = (EntityInstanceWrapper) ctx.get(PM_ENTITY_INSTANCE_WRAPPER);
+        final Field field = (Field) ctx.get(PM_FIELD);
+        Object p = ctx.get(PM_FIELD_VALUE);
+        if (p == null) {
+            p = getValue(einstance, field);
+        }
+        final String value = p.toString();
+        return super.visualize("bigstring_converter.jsp?"
+                + "&value=" + value
+                + "&isNull=" + (p == null)
+                + "&withNull=" + getConfig("with-null", "false"));
+    }
+}
