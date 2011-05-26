3c539c90f358aff2e9073888bed71729ee0f0ecb
Jeronimo Paoletti <jeronimo.paoletti@gmail.com>
2011-05-25 23:44:29 -0300

    Fixed show collection converter

--- src/org/jpos/ee/pm/struts/converter/ShowCollectionConverter.java
+++ src/org/jpos/ee/pm/struts/converter/ShowCollectionConverter.java
@@ -1,6 +1,6 @@
 /*
  * jPOS Project [http://jpos.org]
- * Copyright (C) 2000-2010 Alejandro P. Revilla
+ * Copyright (C) 2000-2011 Alejandro P. Revilla
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as
@@ -15,12 +15,12 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-
 package org.jpos.ee.pm.struts.converter;
 
 import java.util.Collection;
 import org.jpos.ee.pm.converter.Converter;
 import org.jpos.ee.pm.converter.ConverterException;
+import org.jpos.ee.pm.converter.IgnoreConvertionException;
 
 import org.jpos.ee.pm.core.EntityInstanceWrapper;
 import org.jpos.ee.pm.core.Field;
@@ -38,24 +38,27 @@ import org.jpos.ee.pm.core.PMContext;
  * */
 public class ShowCollectionConverter extends Converter {
 
+    @Override
     public Object build(PMContext ctx) throws ConverterException {
-        return ctx.get(PM_FIELD_VALUE);
+        throw new IgnoreConvertionException();
     }
 
+    @Override
     public String visualize(PMContext ctx) throws ConverterException {
         try {
-            EntityInstanceWrapper einstance = (EntityInstanceWrapper) ctx.get(PM_ENTITY_INSTANCE_WRAPPER);
-            Field field = (Field) ctx.get(PM_FIELD);
-            Collection<?> list = (Collection<?>)getValue(einstance, field);
-            StringBuilder sb = new StringBuilder();
+            final EntityInstanceWrapper einstance = (EntityInstanceWrapper) ctx.get(PM_ENTITY_INSTANCE_WRAPPER);
+            final Field field = (Field) ctx.get(PM_FIELD);
+            final Collection<?> list = (Collection<?>) getValue(einstance, field);
+            final StringBuilder sb = new StringBuilder();
             sb.append("<ul>");
-            for(Object o:list){
+            for (Object o : list) {
                 sb.append("<li>");
                 sb.append(o.toString());
                 sb.append("</li>");
             }
             sb.append("</ul>");
-            return sb.toString();
+            ctx.put(PM_FIELD_VALUE,sb.toString());
+            return super.visualize("void.jsp?",null);
         } catch (Exception e1) {
             getPresentationManager().error(e1);
             throw new ConverterException("pm_core.converter.not.collection");
