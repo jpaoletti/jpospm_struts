3e0a2dbe3cf86831d81dc8cd239403540e1b28e7
Jeronimo Paoletti <jeronimo.paoletti@gmail.com>
2011-06-15 12:05:31 -0300

    New Single aggregation converter based on jSON and javascript and avoiding big html code on large selects. Also a new property "min-search-size" forces user to make a search before displaying the select in order to make big selects faster.

--- src/org/jpos/ee/pm/struts/converter/EditSingleAggregationConverter.java
+++ src/org/jpos/ee/pm/struts/converter/EditSingleAggregationConverter.java
@@ -17,6 +17,8 @@
  */
 package org.jpos.ee.pm.struts.converter;
 
+import com.google.gson.Gson;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.jpos.ee.pm.converter.ConverterException;
@@ -30,12 +32,14 @@ import org.jpos.ee.pm.struts.PMStrutsContext;
  * <converter class="org.jpos.ee.pm.converter.EditSingleAggregationConverter">
  *     <operationId>edit</operationId>
  *         <properties>
- *             <property name="entity"         value="the_other_entity" />
- *             <property name="with-null"      value="true" />
- *             <property name="filter"         value="org.jpos.ee.pm.core.ListFilterXX" />
- *             <property name="sort-field"     value="xxx" />
- *             <property name="sort-direction" value="asc | desc" />
- *             <property name="search"         value="true | false" />
+ *             <property name="entity"          value="the_other_entity" />
+ *             <property name="with-null"       value="true" />
+ *             <property name="filter"          value="org.jpos.ee.pm.core.ListFilterXX" />
+ *             <property name="filter"          value="org.jpos.ee.pm.core.ListFilterXX" />
+ *             <property name="sort-field"      value="xxx" />
+ *             <property name="sort-direction"  value="asc | desc" />
+ *             <property name="search"          value="true | false" />
+ *             <property name="min-search-size" value="0" />
  *         </properties>
  * </converter>
  * }
@@ -55,7 +59,7 @@ public class EditSingleAggregationConverter extends AbstractCollectionConverter 
             if (x == -1) {
                 return null;
             }
-            List<?> list = recoverList((PMStrutsContext) ctx, getConfig("entity"), true);
+            final List<?> list = recoverList((PMStrutsContext) ctx, getConfig("entity"), true);
             return list.get(x);
         } catch (Exception e1) {
             ctx.getPresentationManager().error(e1);
@@ -71,16 +75,68 @@ public class EditSingleAggregationConverter extends AbstractCollectionConverter 
         final String entity = getConfig("entity");
         final Field field = (Field) ctx.get(PM_FIELD);
         saveList((PMStrutsContext) ctx, entity);
+
+        final List<?> list = recoverList((PMStrutsContext) ctx, entity, false);
+        final List<ACListItem> finalist = new ArrayList<ACListItem>();
+        if(withNull){
+            finalist.add(new ACListItem(-1, "", false));
+        }
+        for (Object object : list) {
+            if (object != null) {
+                finalist.add(new ACListItem(list.indexOf(object), object.toString(), object.equals(ctx.get(PM_FIELD_VALUE))));
+            }
+        }
+        Gson gson = new Gson();
+        ctx.put("json_list", gson.toJson(finalist));
+        System.out.println(ctx.get("json_list"));
         return super.visualize("single_aggregation_converter.jsp"
                 + "?filter=" + filter
                 + "&entity=" + entity
                 + "&with_null=" + withNull
                 + "&show_search=" + hasSearch(ctx)
+                + "&min_search_size=" + getConfig("min-search-size", "0")
                 + "&prop=" + field.getProperty());
     }
 
     private boolean hasSearch(PMContext ctx) {
         final String s = getConfig("search", "false");
         return ("true".equals(s));
+    }
+
+    public static class ACListItem {
+
+        private int id;
+        private String text;
+        private boolean selected;
+
+        public ACListItem(int id, String text, boolean selected) {
+            this.id = id;
+            this.text = text;
+            this.selected = selected;
+        }
+
+        public int getId() {
+            return id;
+        }
+
+        public void setId(int id) {
+            this.id = id;
+        }
+
+        public boolean isSelected() {
+            return selected;
+        }
+
+        public void setSelected(boolean selected) {
+            this.selected = selected;
+        }
+
+        public String getText() {
+            return text;
+        }
+
+        public void setText(String text) {
+            this.text = text;
+        }
     }
 }
--- webapps/pm/converters/single_aggregation_converter.jsp
+++ webapps/pm/converters/single_aggregation_converter.jsp
@@ -1,48 +1,32 @@
-<%--
- * jPOS Project [http://jpos.org]
- * Copyright (C) 2000-2011 Alejandro P. Revilla
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Affero General Public License as
- * published by the Free Software Foundation, either version 3 of the
- * License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Affero General Public License for more details.
- *
- * You should have received a copy of the GNU Affero General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
---%>
 <%@include file="../inc/tag-libs.jsp" %>
-<bean:define id="collection" name="ctx"    property="tmpList" type="java.util.List" />
+<bean:define id="ctx" name="ctx" type="org.jpos.ee.pm.core.PMContext" />
+<script type="text/javascript">
+    if(typeof saclists == "undefined") var saclists = new Array();
+    if(typeof sacupds == "undefined") var sacupds = new Array();
+    saclists["${param.f}"] = jQuery.parseJSON( '${ctx.map.json_list}' );
+    sacupds["${param.f}"] = function(){
+        $("#f_${param.f}").contents().each(function(){$(this).remove()});
+        var list = saclists["${param.f}"];
+        var j = 0;
+        var filter = "";
+        if(typeof $("#search_${param.f}").val() != "undefined"){
+            filter=$("#search_${param.f}").val();
+        }
+        if(filter.length >= ${param.min_search_size}){
+            for(i=0; i < list.length ; i++){
+                pr = list[i];
+                var match = pr.text.search(new RegExp(filter, "i"));
+                if(match >= 0){
+                    $("#f_${param.f}").get(0)[j]= new Option(list[i].text, list[i].id, false, list[i].selected);
+                    j++;
+                }
+            }
+        }
+    };
+</script>
 <c:if test="${param.show_search}">
     <input type="text" id="search_${param.f}" size="7" /> &nbsp;
-    <script type="text/javascript">
-        $("#search_${param.f}").change(function() {
-            var filter = $(this).val();
-            //alert(filter);
-            $("#f_${param.f} option").each(function() {
-                var match = $(this).text().search(new RegExp(filter, "i"));
-                if (match < 0 && $(this).text() != "")  {
-                    $(this).attr("disabled",true);
-                    $(this).hide();
-                }else{
-                    $(this).show();
-                    $(this).attr("disabled",false);
-                }
-
-            });
-        });
-    </script>
+    <script type="text/javascript">$("#search_${param.f}").change(sacupds["${param.f}"]);</script>
 </c:if>
-<select size="1" id="f_${param.f}" name="f_${param.f}">
-    <c:if test="${param.with_null}">
-        <option value="-1" />&nbsp;<br/>
-    </c:if>
-    <logic:iterate id="o" name="collection" type="java.lang.Object" indexId="i">
-        <bean:define id="checked" value="${ o eq ctx.map.PM_FIELD_VALUE ? 'selected' : ''}" />
-        <option ${checked} value="${i}" />&nbsp;${o}<br/>
-    </logic:iterate>
-</select>
+<select size="1" id="f_${param.f}" name="f_${param.f}"></select>
+<script type="text/javascript">sacupds["${param.f}"]();</script>
