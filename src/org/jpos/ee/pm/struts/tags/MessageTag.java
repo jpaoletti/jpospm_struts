71032504e18180a1d5162cd548c9645d4355424d
Jeronimo Paoletti <jeronimo.paoletti@gmail.com>
2011-09-15 15:35:42 -0300

    new pmfn:message tag comes to replaces both struts bean:message and pm:message tags.

---
+++ src/org/jpos/ee/pm/struts/tags/MessageTag.java
@@ -0,0 +1,104 @@
+/*
+ * jPOS Project [http://jpos.org]
+ * Copyright (C) 2000-2011 Alejandro P. Revilla
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.jpos.ee.pm.struts.tags;
+
+import javax.servlet.jsp.JspException;
+import javax.servlet.jsp.JspTagException;
+import javax.servlet.jsp.tagext.TagSupport;
+import org.jpos.ee.pm.core.PresentationManager;
+
+/**
+ * Display an internationalized message
+ *
+ * @author jpaoletti
+ * @since 15/09/2011
+ * @version v1.2
+ *
+ */
+public class MessageTag extends TagSupport {
+
+    private String key;
+    private String arg0;
+    private String arg1;
+    private String arg2;
+    private String arg3;
+    private String arg4;
+
+    @Override
+    public int doStartTag() throws JspException {
+        try {
+            pageContext.getOut().print(PresentationManager.getMessage(getKey(), getArg0(), getArg1(), getArg2(), getArg3(), getArg4()));
+        } catch (Exception ex) {
+            throw new JspTagException("MessageTag: " + ex.getMessage());
+        }
+        return SKIP_BODY;
+    }
+
+    @Override
+    public int doEndTag() {
+        return EVAL_PAGE;
+    }
+
+    public String getArg0() {
+        return arg0;
+    }
+
+    public void setArg0(String arg0) {
+        this.arg0 = arg0;
+    }
+
+    public String getArg1() {
+        return arg1;
+    }
+
+    public void setArg1(String arg1) {
+        this.arg1 = arg1;
+    }
+
+    public String getArg2() {
+        return arg2;
+    }
+
+    public void setArg2(String arg2) {
+        this.arg2 = arg2;
+    }
+
+    public String getArg3() {
+        return arg3;
+    }
+
+    public void setArg3(String arg3) {
+        this.arg3 = arg3;
+    }
+
+    public String getArg4() {
+        return arg4;
+    }
+
+    public void setArg4(String arg4) {
+        this.arg4 = arg4;
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+}
--- webapps/pm/WEB-INF/tags/header.tag
+++ webapps/pm/WEB-INF/tags/header.tag
@@ -1,19 +1,20 @@
 <%@tag description="This tag encapsulates site header" pageEncoding="UTF-8"%>
 <%@ taglib uri="/WEB-INF/tld/c.tld" prefix="c" %>
+<%@ taglib uri="/WEB-INF/tld/pmfn.tld" prefix="pmfn" %>
 <%@ taglib tagdir="/WEB-INF/tags" prefix="pm" %>
 <div id="header">
     <pm:topmenu />
     <div id="logo">
-        <h1 class="title"><a href="${es.context_path}"><pm:message key="${pm.title}"/></a></h1>
-        <h2 class="title"><pm:message key="${pm.subtitle}"/></h2>
+        <h1 class="title"><a href="${es.context_path}"><pmfn:message key="${pm.title}"/></a></h1>
+        <h2 class="title"><pmfn:message key="${pm.subtitle}"/></h2>
     </div>
     <c:if test="${not empty pmsession.user}">
-    <div id="userbox">
-        <img src='${pmsession.user.gravatar}?s=50&d=mm' alt="gravatar"/> &nbsp;${pmsession.user.name} &nbsp; |
-        <c:if test="${pm.loginRequired}">
-        &nbsp; <a href="javascript:loadPage('${es.context_path}/show.do?pmid=secuserprofile&identified=username:${pmsession.user.username}');"><pm:message key="user.profile"/></a> &nbsp; |
-        </c:if>
-        &nbsp; <a href="${es.context_path}/logout.do" title="<pm:message key="logout"/>"><pm:message key="logout"/></a>
-    </div>
+        <div id="userbox">
+            <img src='${pmsession.user.gravatar}?s=50&d=mm' alt="gravatar"/> &nbsp;${pmsession.user.name} &nbsp; |
+            <c:if test="${pm.loginRequired}">
+                &nbsp; <a href="javascript:loadPage('${es.context_path}/show.do?pmid=secuserprofile&identified=username:${pmsession.user.username}');"><pmfn:message key="user.profile"/></a> &nbsp; |
+            </c:if>
+            &nbsp; <a href="${es.context_path}/logout.do" title="<pmfn:message key="logout"/>"><pmfn:message key="logout"/></a>
+        </div>
     </c:if>
 </div>
